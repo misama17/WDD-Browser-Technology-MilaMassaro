cb3aca33420d2b7e6ff5149c40efdc98b2d78db9
misama17 <milamassaro17@gmail.com>
2026-03-10 16:02:40 +0100

    update dinsdag week 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 // MARK: Constanten
 const form = document.querySelector("form")
-const errorID = document.querySelector("#errorID")
-
 const inputs = form.querySelectorAll("input")
 
 
@@ -15,27 +13,37 @@ form.setAttribute('novalidate', '')
 // MARK: Functions
 // Aantekeningen van workshop Victor (zie Notion: https://www.notion.so/Browser-Technology-3-6-30c4a28d1c2c8008a1f6d3a744cb105a?source=copy_link) 
 // en https://codepen.io/3hos15/pen/PwGGGRM
+
 // ---------- Validity Check ----------
 form.addEventListener('submit', event => {
-	// Stopt verzending van formulier als het niet correct is ingevuld/voor de check
-	event.preventDefault()
-	
 	// Hiermee zorg je dat er wel wat report wordt
 	// event.target.reportValidity() 
 
 
     inputs.forEach(input => {
         if (!input.checkValidity()) {
+            // Stopt verzending van formulier als het niet correct is ingevuld/voor de check
+            event.preventDefault()
+
             input.setAttribute("aria-describedby", "errorID")
-            errorID.textContent = "veld is niet goed hoor"
+
+            // Vasilis: parentNode neemt het bovenliggende element in HTML DOM
+            let errorMsg = input.parentNode.querySelector('p.error-msg');
+
+            // extra if-statement, omdat hij het moet doen als er een p met class=error aanwezig is.
+            if(errorMsg){
+                errorMsg.textContent = "Dit veld is niet correct ingevuld."
+            
+                input.setAttribute("aria-invalid", "true")
+            }
+            
 
             // Direct focus naar waar de fout zit (- workshop Victor, zie aantekeningen)
             // input.focus()
 
-            // VRAAG: aria-invalid true toevoegen en de volgende weer weghalen??
-            input.setAttribute("aria-invalid", "true")
         } else {
             errorID.textContent = ""
+            input.setAttribute("aria-invalid", "false")
         }
     })
 
@@ -47,7 +55,9 @@ form.addEventListener('submit', event => {
 inputs.forEach(input => {
     input.addEventListener("input", () => {
         if (input.checkValidity()) {
-            errorID.textContent = ""
+            let errorMsg = input.parentNode.querySelector('p.error-msg');
+
+            errorMsg.textContent = ""
             input.setAttribute("aria-invalid", "false")
         } 
     })
