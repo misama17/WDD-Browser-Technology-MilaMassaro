cee1ddb4fb60857a9389f15c0b4a325d5ef512d9
misama17 <milamassaro17@gmail.com>
2026-03-17 10:12:20 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -4,16 +4,26 @@ const inputs = form.querySelectorAll("input")
 
 
 
-
 // Zet automatische "pop-up" van browser uit.
 form.setAttribute('novalidate', '')
 
 
-
 // MARK: Functions
+// https://developer.mozilla.org/en-US/docs/Web/API/ValidityState/typeMismatch
+function errorMsg(text, id) {
+    const logElement = document.getElementById(id+"-error")
+
+    // Je wilt dat deze er pas is als er sprake is van een id met error erachter (dus als het niet valide is)
+    if (logElement) {
+        logElement.innerText = text;
+    }
+}
+
+
+
 // Aantekeningen van workshop Victor (zie Notion: https://www.notion.so/Browser-Technology-3-6-30c4a28d1c2c8008a1f6d3a744cb105a?source=copy_link) 
 // en https://codepen.io/3hos15/pen/PwGGGRM
-
+// MARK: Input velden
 // ---------- Validity Check ----------
 form.addEventListener('submit', event => {
 	// Hiermee zorg je dat er wel wat report wordt
@@ -25,45 +35,47 @@ form.addEventListener('submit', event => {
             // Stopt verzending van formulier als het niet correct is ingevuld/voor de check
             event.preventDefault()
             
-            //input.validity.valid
-            //true of false
-            //input.validity.valueMissing
-            //https://developer.mozilla.org/en-US/docs/Web/API/ValidityState
 
-            input.setAttribute("aria-describedby", "errorID")
-
-            // Vasilis: parentNode neemt het bovenliggende element in HTML DOM
-            let errorMsg = input.parentNode.querySelector('p.error-msg');
-
-            // extra if-statement, omdat hij het moet doen als er een p met class=error aanwezig is.
-            if(errorMsg){
-                errorMsg.textContent = "Dit veld is niet correct ingevuld."
+            input.setAttribute("aria-describedby", input.id+"-error")
             
+            //https://developer.mozilla.org/en-US/docs/Web/API/ValidityState
+            if (input.validity.valueMissing) {
+                errorMsg("Dit veld is verplicht", input.id)
                 input.setAttribute("aria-invalid", "true")
+            
+            } else if (input.validity.patternMismatch) {
+                errorMsg("Voer een geldige waarde in", input.id)
+                input.setAttribute("aria-invalid", "true")
+
             }
             
 
-            // Direct focus naar waar de fout zit (- workshop Victor, zie aantekeningen)
-            // input.focus()
+            // Direct focus naar waar fout zit (- workshop Victor, zie aantekeningen)
+            // VRAAG: Hoe naar bovenste foute input?
+            input.focus()
 
-        } else {
-            errorID.textContent = ""
+        }
+         else {
+            errorMsg("", input.id)
             input.setAttribute("aria-invalid", "false")
+            input.removeAttribute("aria-describedby")
         }
     })
 
 })
 
 
-
-// ---------- Remove error message when filled in correctly ----------
+// Haalt de error styling weg als er wél een valide input is
 inputs.forEach(input => {
     input.addEventListener("input", () => {
         if (input.checkValidity()) {
-            let errorMsg = input.parentNode.querySelector('p.error-msg');
-
-            errorMsg.textContent = ""
+            errorMsg("", input.id)
+            console.log(errorMsg)
             input.setAttribute("aria-invalid", "false")
         } 
     })
 })
+
+
+
+// MARK: Radio buttons
